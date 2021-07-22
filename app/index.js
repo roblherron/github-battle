449a35d479501f5268bfb42ce7aa5fa6a37a1ea2
robherron <rlherron100@gmail.com>
2021-07-22 00:41:14 -0400

    Created Navbar Component

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Popular from './components/Popular';
 
-class App extends React.Component {
+
+
+export default class App extends React.Component {
     render() {
         return ( 
-        <div>Hello World!</div>
+        <div className="container">
+            <Popular/>
+        </div>
         )}
 }
 
